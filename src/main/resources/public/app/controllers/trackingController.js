9b5854daffc83ce1354e3c40971cc023d9b49be8
Ken Little <klittle@shelterinsurance.com>
2017-04-28 09:17:19 -0500

    Adding a controller and data model to display.

---
+++ src/main/resources/public/app/controllers/trackingController.js
@@ -0,0 +1,5 @@
+trackingApp.controller('trackingController', ['$scope', '$http', function($scope, $http) {
+        $scope.salutations = "Hey there ";
+        $scope.name = "Jim";
+        $scope.punctuation = "!";
+    }]);
